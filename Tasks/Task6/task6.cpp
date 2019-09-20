055331c1fe6dbe2b491b29fb78ab0fe7ce57af73
vladimirlisovskij <vovalisovskij@yandex.ru>
2019-09-20 17:05:36 +0300

    add 1-5 tasks

---
+++ Tasks/Task6/task6.cpp
@@ -0,0 +1,23 @@
+//
+// Created by vladimir on 20.09.2019.
+//
+
+#include <iostream>
+#include <cmath>
+
+using  namespace std;
+
+ int  main(){
+    double a,b,c;
+    cin >> a >> b >> c;
+    if (!a){
+        (!b ? cout << "Корней нет" : cout << "X = " << -c / b) << endl;
+    }else{
+        double D = b*b - 4*a*c;
+        if (D<0){
+            cout << "Корней нет" << endl;
+        }else !D ? cout << "X = " << -b / 2 / a << endl : cout << "X1 = " << (-b + sqrt(D)) / 2 / a << endl << "X1 = "
+                                                                 << (-b - sqrt(D)) / 2 / a << endl;
+    }
+    return 0;
+}
