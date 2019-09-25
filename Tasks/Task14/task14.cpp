5901b66367f406dfa2fcb3a2d8a8b396a0ca780f
vladimirlisovskij <vovalisovskij@yandex.ru>
2019-09-25 17:03:11 +0300

    lorem ipsum

---
+++ Tasks/Task14/task14.cpp
@@ -0,0 +1,32 @@
+//
+// Created by vladimir on 25.09.2019.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int s, l1, r1, l2, r2;
+
+    cin >> s >> l1 >> r1 >> l2 >> r2;
+
+    int res1, res2;
+    bool stop = false;
+
+    for (int i = l1; i <= r1; i++){
+        for (int j = l2; j <= r2; j++){
+            cout << i + j << endl;
+            if (i + j == s){
+                res1 = i;
+                res2 = j;
+                stop = true;
+                break;
+            } else if (i + j > s) break;
+        }
+        if (stop) break;
+    }
+
+    if (stop) cout << res1 << ' ' << res2 << endl;
+    else cout << -1;
+}
